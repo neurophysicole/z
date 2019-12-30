c3e0207839624eabb08b776ffaf999cff26ab718
neurophysicole <48078188+neurophysicole@users.noreply.github.com>
2019-12-30 04:33:21 -0600

    updated to fix minor issues with backing up to the cloud

--- z_scripts/cloud_update.py
+++ z_scripts/cloud_update.py
@@ -34,9 +34,9 @@ def cloud_update(main_dir, backup_dir, segment, logfile):
 
     # set files
     local_log       = open(local_logfile, 'a+')
-    local_log       = local_log.readlines()
+    local_log       = local_log.read()
     cloud_log       = open(cloud_logfile, 'a+')
-    cloud_log       = cloud_log.readlines()
+    cloud_log       = cloud_log.read()
 
     # update to the cloud
     # -------------------
@@ -48,14 +48,14 @@ def cloud_update(main_dir, backup_dir, segment, logfile):
         # logit
         for line in local_log:
             if line not in cloud_log:
-                cloud_log.append(line)
+                cloud_log.write(line)
         for line in cloud_log:
             if line not in local_log:
                 local_log.write(line)
 
     # close the files
-    local_log.close()
-    cloud_log.close()
+    # local_log.close()
+    # cloud_log.close()
 
     # ===============
     # Run the update
@@ -92,9 +92,9 @@ def cloud_update(main_dir, backup_dir, segment, logfile):
             cloud_proj_log  = '%s/%s/%s/%s' %(backup_dir, segment, proj, logfile)
 
             local_proj_log  = open(local_proj_log, 'a+')
-            local_proj_log  = local_proj_log.readlines()
+            local_proj_log  = local_proj_log.read()
             cloud_proj_log  = open(cloud_proj_log, 'a+')
-            cloud_proj_log  = local_proj_log.readlines()
+            cloud_proj_log  = cloud_proj_log.read()
 
             if set(local_proj_log) == set(cloud_proj_log):
                 print('\nProject log files up to date locally and on the cloud.\n')
@@ -107,8 +107,8 @@ def cloud_update(main_dir, backup_dir, segment, logfile):
                     if line not in local_proj_log:
                         local_proj_log.write(line)
 
-            local_proj_log.close()
-            cloud_proj_log.close()
+            # local_proj_log.close()
+            # cloud_proj_log.close()
 
             # task update
             # -----------
@@ -187,8 +187,8 @@ def cloud_update(main_dir, backup_dir, segment, logfile):
                     local_note_path = '%s/%s/%s/%s' %(main_dir, segment, proj, task)
                     cloud_note_path = '%s/%s/%s/%s' %(backup_dir, segment, proj, task)
 
-                    local_note_list = next(os.walk(local_note_path))[1]
-                    cloud_note_list = next(os.walk(cloud_note_path))[1]
+                    local_note_list = next(os.walk(local_note_path))[2]
+                    cloud_note_list = next(os.walk(cloud_note_path))[2]
 
                     # compare lists
                     note_status = set(local_note_list) == set(cloud_note_list)

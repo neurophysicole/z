d798e1e207c491deeddce84efba2193e291aa835
Zach NeurophysiCole <neurophysicole@gmail.com>
2020-01-21 11:58:47 -0600

    put logfile updating at the end (was messing up cloud updating) had to fix cloud updating (cloud->local) --there might be other bugs like this one, but I don't have the time to fix them at the moment

--- z_scripts/cloud_update.py
+++ z_scripts/cloud_update.py
@@ -17,86 +17,9 @@ def cloud_update(main_dir, backup_dir, cur_branch_name, logfile):
     print('Checking the local and cloud files for asymmetries..')
     time.sleep(.1)
 
-    # ========================
-    # Update Master Log Sheet
-    # ========================
-    # master
-    # check that the local and cloud log sheets are all up to date
     # establish project paths
     local_proj_path = '%s/%s' %(main_dir, cur_branch_name)
     cloud_proj_path = '%s/%s' %(backup_dir, cur_branch_name)
-    
-    local_master_log        = '%s/%s' %(local_proj_path, logfile)
-    cloud_master_log        = '%s/%s' %(cloud_proj_path, logfile)
-
-    # set files
-    local_master_log_file   = open(local_master_log, 'r')
-    cloud_master_log_file   = open(cloud_master_log, 'r')
-
-    local_master_log_list   = local_master_log_file.read().splitlines()
-    cloud_master_log_list   = cloud_master_log_file.read().splitlines()
-
-    # ------
-    # master
-    # not doing project level because will need to iterate through and check all projects in case updates were made on a different computer
-    cloud_appended = False #for updating
-    local_appended = False #for updating
-
-    # check and update cloud master log list
-    for line in local_master_log_list:
-        if line not in cloud_master_log_list:
-            cloud_master_log_list.append(line)
-            cloud_appended = True
-    
-    # check and update local master log list
-    for line in cloud_master_log_list:
-        if line not in local_master_log_list:
-            local_master_log_list.append(line)
-            local_appended = True 
-    
-    # update local log file
-    if local_appended:
-        local_master_log_file.close()
-        with open(local_master_log, 'w') as local_master_log_file:
-            for line in local_master_log_list:
-                local_master_log_file.write('%s\n' %line)
-
-    # update cloud log file
-    if cloud_appended:
-        cloud_master_log_file.close()
-        with open(cloud_master_log, 'w') as cloud_master_log_file:
-            for line in cloud_master_log_list:
-                cloud_master_log_file.write('%s\n' %line)
-
-    # close out files
-    local_master_log_file.close()
-    cloud_master_log_file.close()
-
-    # ==============
-    # Project Check
-    # ==============
-    
-    # make sure the current branch is in the cloud file system
-    if os.path.isdir(cloud_proj_path):
-        print('Checking %s' %cur_branch_name)
-        time.sleep(.1)
-    else: #there is no branch dir in the cloud file system
-        print('\nGrowing the new %s branch in the cloud.\n' %cur_branch_name)
-        time.sleep(.1)
-
-        # copy the branch up to the cloud
-        os.system('cp -a -v %s %s' %(local_proj_path, backup_dir))
-
-    # make sure the current branch is in the local file system
-    if os.path.isdir(local_proj_path):
-        print('Ya, what he said..')
-        time.sleep(.1)
-    else: #there is not branch dir in the local file system
-        print('\nGrowing the new %s local branch.\n' %cur_branch_name)
-        time.sleep(.1)
-
-        # copy the branch down to the local file system
-        os.system('cp -a -v %s %s' %(cloud_proj_path, main_dir))
 
     # create project lists
     local_proj_list = next(os.walk(local_proj_path))[1]
@@ -108,67 +31,7 @@ def cloud_update(main_dir, backup_dir, cur_branch_name, logfile):
     local_archive_proj_list = next(os.walk(local_archive_proj_path))[1]
     cloud_archive_proj_list = next(os.walk(cloud_archive_proj_path))[1]
 
-    # get local master logfile information
-    local_master_log = '%s/log.txt' %(local_proj_path) #find it
-    read_local_master_log = open(local_master_log, 'r') #open it
-    local_master_log_list = read_local_master_log.read().splitlines() #read it in
-
-    # get cloud master logfile information
-    cloud_master_log = '%s/log.txt' %(cloud_proj_path) #find it
-    read_cloud_master_log = open(cloud_master_log, 'r') #open it
-    cloud_master_log_list = read_cloud_master_log.read().splitlines() #read it in
-    
-    print('Updating projects..')
-    time.sleep(.1)
-
-    # =========================
-    # Check Local Project List
-    # =========================
     for proj in local_proj_list:
-
-        # log update
-        # ----------
-        # project log
-        local_proj_log  = '%s/%s/%s' %(local_proj_path, proj, logfile)
-        cloud_proj_log  = '%s/%s/%s' %(cloud_proj_path, proj, logfile)
-
-        local_proj_log_file     = open(local_proj_log, 'r')
-        cloud_proj_log_file     = open(cloud_proj_log, 'r')
-
-        local_proj_log_list     = local_proj_log_file.read().splitlines()
-        cloud_proj_log_list     = cloud_proj_log_file.read().splitlines()
-
-        # compare log files, update if necessary
-        # update cloud list
-        local_appended = False # for updating
-        cloud_appended = False # for updating
-
-        for line in local_proj_log_list:
-            if line not in cloud_proj_log_list:
-                cloud_proj_log_list.append(line)
-                cloud_appended = True
-        
-        # update local list
-        for line in cloud_proj_log_list:
-            if line not in local_proj_log_list:
-                local_proj_log_list.append(line)
-                local_appended = True
-        
-        # if local list was changed
-        if local_appended:
-            local_proj_log_file.close()
-            with open(local_proj_log, 'w') as local_proj_log_file:
-                for line in local_proj_log_list:
-                    local_proj_log_file.write('%s\n' %line)
-        
-        # if cloud list was changed
-        if cloud_appended:
-            cloud_proj_log_file.close()
-            with open(cloud_proj_log, 'w') as cloud_proj_log_file:
-                for line in cloud_proj_log_list:
-                    cloud_proj_log_file.write('%s\n' %line)
-
-
         # local project info
         local_task_path = '%s/%s' %(local_proj_path, proj)
         local_task_list = next(os.walk(local_task_path))[1]
@@ -403,21 +266,19 @@ def cloud_update(main_dir, backup_dir, cur_branch_name, logfile):
                 # --------------------------
                 # check active tasks (local)
                 if os.path.isdir(local_note_path):
-
                     # if task exists locally, check the notes
-                    local_note_list = next(os.walk(local_note_path))[2]                
-                    for note in local_note_list:
+                    for note in cloud_note_list:
                         # evaporate to the cloud, then precipitate back down
                         local_note = '%s/%s' %(local_note_path, note)
                         cloud_note = '%s/%s' %(cloud_note_path, note)
-                        if not os.path.isfile(cloud_note):
+                        if not os.path.isfile(local_note):
                             # evaporate to the cloud
-                            os.system('cp -v %s %s' %(local_note, cloud_note_path))
+                            os.system('cp -v %s %s' %(cloud_note, local_note_path))
                             update = True
 
                     if update:
                         print('\nPrecipitating %s-%s down from the cloud.\n' %(proj, task))
-                        os.system('cp -a -v %s %s' %(cloud_note_path, local_task_path))
+                        os.system('cp -a -v %s %s' %(local_note_path, cloud_task_path))
                         time.sleep(.1)
                         update = False
 
@@ -489,18 +350,18 @@ def cloud_update(main_dir, backup_dir, cur_branch_name, logfile):
                 if os.path.isdir(local_archive_note_path):
 
                     # if exists in local archive, update notes
-                    for note in local_archive_note_list:
+                    for note in cloud_archive_note_list:
                         # evaporate to the cloud then precipitate back down
                         local_archive_note = '%s/%s' %(local_archive_note_path, note)
                         cloud_archive_note = '%s/%s' %(cloud_archive_note_path, note)
-                        if not os.path.isfile(cloud_archive_note):
+                        if not os.path.isfile(local_archive_note):
                             # evaporate to the cloud
-                            os.system('cp -v %s %s' %(local_archive_note, cloud_archive_note_path))
+                            os.system('cp -v %s %s' %(cloud_archive_note, local_archive_note_path))
                             update = True
                     
                     if update:
                         print('\nPrecipitating %s-%s down to the local archive.\n' %(proj, task))
-                        os.system('cp -a -v %s %s' %(cloud_archive_note_path, local_archive_task_path))
+                        os.system('cp -a -v %s %s' %(local_archive_note_path, cloud_archive_task_path))
                         time.sleep(.1)
                         update = False
 
@@ -703,3 +564,139 @@ def cloud_update(main_dir, backup_dir, cur_branch_name, logfile):
                     os.system('cp -a -v %s/%s %s' %(cloud_archive_proj_path, proj, local_archive_proj_path))
                     os.system('rm -r -v -f %s/%s' %(local_proj_path, proj))
                 
+    # ========================
+    # Update Master Log Sheet
+    # ========================
+    # master
+    # check that the local and cloud log sheets are all up to date
+    local_master_log        = '%s/%s' %(local_proj_path, logfile)
+    cloud_master_log        = '%s/%s' %(cloud_proj_path, logfile)
+
+    # set files
+    local_master_log_file   = open(local_master_log, 'r')
+    cloud_master_log_file   = open(cloud_master_log, 'r')
+
+    local_master_log_list   = local_master_log_file.read().splitlines()
+    cloud_master_log_list   = cloud_master_log_file.read().splitlines()
+
+    # ------
+    # master
+    # not doing project level because will need to iterate through and check all projects in case updates were made on a different computer
+    cloud_appended = False #for updating
+    local_appended = False #for updating
+
+    # check and update cloud master log list
+    for line in local_master_log_list:
+        if line not in cloud_master_log_list:
+            cloud_master_log_list.append(line)
+            cloud_appended = True
+    
+    # check and update local master log list
+    for line in cloud_master_log_list:
+        if line not in local_master_log_list:
+            local_master_log_list.append(line)
+            local_appended = True 
+    
+    # update local log file
+    if local_appended:
+        local_master_log_file.close()
+        with open(local_master_log, 'w') as local_master_log_file:
+            for line in local_master_log_list:
+                local_master_log_file.write('%s\n' %line)
+
+    # update cloud log file
+    if cloud_appended:
+        cloud_master_log_file.close()
+        with open(cloud_master_log, 'w') as cloud_master_log_file:
+            for line in cloud_master_log_list:
+                cloud_master_log_file.write('%s\n' %line)
+
+    # close out files
+    local_master_log_file.close()
+    cloud_master_log_file.close()
+
+    # ==============
+    # Project Check
+    # ==============
+    
+    # make sure the current branch is in the cloud file system
+    if os.path.isdir(cloud_proj_path):
+        print('Checking %s' %cur_branch_name)
+        time.sleep(.1)
+    else: #there is no branch dir in the cloud file system
+        print('\nGrowing the new %s branch in the cloud.\n' %cur_branch_name)
+        time.sleep(.1)
+
+        # copy the branch up to the cloud
+        os.system('cp -a -v %s %s' %(local_proj_path, backup_dir))
+
+    # make sure the current branch is in the local file system
+    if os.path.isdir(local_proj_path):
+        print('Ya, what he said..')
+        time.sleep(.1)
+    else: #there is not branch dir in the local file system
+        print('\nGrowing the new %s local branch.\n' %cur_branch_name)
+        time.sleep(.1)
+
+        # copy the branch down to the local file system
+        os.system('cp -a -v %s %s' %(cloud_proj_path, main_dir))
+
+    # get local master logfile information
+    local_master_log = '%s/log.txt' %(local_proj_path) #find it
+    read_local_master_log = open(local_master_log, 'r') #open it
+    local_master_log_list = read_local_master_log.read().splitlines() #read it in
+
+    # get cloud master logfile information
+    cloud_master_log = '%s/log.txt' %(cloud_proj_path) #find it
+    read_cloud_master_log = open(cloud_master_log, 'r') #open it
+    cloud_master_log_list = read_cloud_master_log.read().splitlines() #read it in
+    
+    print('Updating projects..')
+    time.sleep(.1)
+
+    # =========================
+    # Check Local Project List
+    # =========================
+    for proj in local_proj_list:
+
+        # log update
+        # ----------
+        # project log
+        local_proj_log  = '%s/%s/%s' %(local_proj_path, proj, logfile)
+        cloud_proj_log  = '%s/%s/%s' %(cloud_proj_path, proj, logfile)
+
+        local_proj_log_file     = open(local_proj_log, 'r')
+        cloud_proj_log_file     = open(cloud_proj_log, 'r')
+
+        local_proj_log_list     = local_proj_log_file.read().splitlines()
+        cloud_proj_log_list     = cloud_proj_log_file.read().splitlines()
+
+        # compare log files, update if necessary
+        # update cloud list
+        local_appended = False # for updating
+        cloud_appended = False # for updating
+
+        for line in local_proj_log_list:
+            if line not in cloud_proj_log_list:
+                cloud_proj_log_list.append(line)
+                cloud_appended = True
+        
+        # update local list
+        for line in cloud_proj_log_list:
+            if line not in local_proj_log_list:
+                local_proj_log_list.append(line)
+                local_appended = True
+        
+        # if local list was changed
+        if local_appended:
+            local_proj_log_file.close()
+            with open(local_proj_log, 'w') as local_proj_log_file:
+                for line in local_proj_log_list:
+                    local_proj_log_file.write('%s\n' %line)
+        
+        # if cloud list was changed
+        if cloud_appended:
+            cloud_proj_log_file.close()
+            with open(cloud_proj_log, 'w') as cloud_proj_log_file:
+                for line in cloud_proj_log_list:
+                    cloud_proj_log_file.write('%s\n' %line)
